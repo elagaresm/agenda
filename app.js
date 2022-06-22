cec40a4016eaa64e054fb92edbed39d7fc806eac
Albert2707 <202010652@itla.edu.do>
2022-06-21 23:14:05 -0400

    cambios

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const response = fetch('http://www.raydelto.org/agenda.php')
     .then(response => response.json())
     .then(data => {
         let contactList = document.getElementById('contactList')
+        var numero = 1
         for (let contacto of data) {
             let list = document.createElement('li')
+            numero++;
+            console.log(numero)
+            if(numero%2 == 0){
+                list.setAttribute('class',' sticky top-0 absolute bg-gray-200 text-center text-slate-800 text-xl font-semibold')
+            }else{
+                list.setAttribute('class',' sticky top-0 absolute bg-white text-center text-slate-800 text-xl font-semibold')
+            }
+   
             list.innerHTML = contacto.nombre
             contactList.appendChild(list)
         }
